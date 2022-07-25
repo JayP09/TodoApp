3694fe87b5dbca35e027d813df55299199625df8
Jay Panchal <panchalj272@gmail.com>
2022-07-25 22:12:04 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import { FaPlus,FaTrash } from 'react-icons/fa';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-col items-center h-screen bg-gradient-to-br from-[#ec2c2c] to-[#e9993d] text-white">
+      <h1 className="text-4xl mx-6 mt-6 uppercase font-bold">Add Todo List</h1>
+      <div className="border-t-4 border-white w-8 mb-5 mt-2"></div>
+      <div className="flex w-full justify-center">
+        <div className="flex justify-center items-center w-[75%] lg:w-[60%] xl:w-[50%]">
+          <input className="p-2 text-base float-left w-full" type="text" placeholder="Add Task..."/>
+          <div className="flex items-center justify-center p-2 text-base bg-[#e9993d] h-full">
+            <FaPlus />
+          </div>
+          <div className="flex items-center justify-center p-2 text-base bg-[#ec2c2c] h-full">
+            <FaTrash />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
